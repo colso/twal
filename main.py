8443c718d4b2601e44b0c3d03144c89c3988811d
root <root@mars.(none)>
2019-07-03 18:50:23 +0900

    delete :

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def main():
 	ctp = tdb.candi_tbl()
 	ctp.set_db_connection(db_conn)
 	for key in m_dic.keys():
-		ret, result =  ctp.torr_candi_search(key):
+		ret, result =  ctp.torr_candi_search(key)
 		if result:
 			print("This magnet already exist in table. ({})".format(key))
 		else:
